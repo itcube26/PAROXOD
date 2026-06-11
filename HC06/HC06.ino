0efde012d8317c98b064e0a92b5c78d7556a3ad9
itcube26 <lencodigitexer@disroot.org>
2026-06-11 17:59:00 +0300

    Add files via upload

--- HC06/HC06.ino
+++ HC06/HC06.ino
@@ -1,9 +1,40 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <GyverPing.h>
+/*
+ПЕРЕДНИЙ ДАТЧИК:
+#define HC_TRIG 38
+#define HC_ECHO 39
 
+ЛЕВЫЙ ДАТЧИК:
+#define HC_TRIG 31
+#define HC_ECHO 32
+
+ПРАВЫЙ ДАТЧИК:
+#define HC_TRIG 23
+#define HC_ECHO 22
+
+*/
+
+
+
+#define HC_TRIG 23
+#define HC_ECHO 22
+
+GPingSync sonar(HC_TRIG, HC_ECHO);
+
+void setup() {
+    Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // сделать измерение
+    sonar.ping();
 
+    // открой плоттер для вывода графика
+    Serial.print(sonar.getRaw());       // "сырое" значение
+    Serial.print(',');
+    Serial.print(sonar.getFiltered());  // фильтрованное от резких выбросов
+    Serial.print(',');
+    Serial.println(sonar.getSmooth());  // + усреднение, плавный точный сигнал
+
+    delay(50);
 }
